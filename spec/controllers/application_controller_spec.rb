2c6af18909be91139b0521011cccc6c1a3de6849
Trung Lê <trung.le@ruby-journal.com>
2014-03-24 09:17:06 +1100

    Add spec for ApplicationController#authenticate!

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,10 +1,35 @@
 require 'spec_helper'
 
 describe ApplicationController do
-
   controller do
     def index
-      render text: 'Hello World'
+      render text: "#{current_user.id}"
+    end
+  end
+
+  describe '#current_user' do
+    context 'when user is not signed in' do
+      before do
+        session[:user_id] = nil
+        get :index
+      end
+
+      it 'returns nil' do
+        expect(controller.current_user).to be_nil
+      end
+    end
+
+    context 'when user is signed in' do
+      let!(:user) { UserCreation.create(email: 'hello@example.com', password: '123', password_confirmation: '123') }
+
+      before do
+        session[:user_id] = user.id
+        get :index
+      end
+
+      it 'returns signed-in user' do
+        expect(controller.current_user).to eq user
+      end
     end
   end
 
@@ -32,6 +57,5 @@ describe ApplicationController do
         expect(response).not_to redirect_to(login_url)
       end
     end
-
   end
 end
